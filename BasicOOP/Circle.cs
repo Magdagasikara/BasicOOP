7b98e54bb91c6a4cc7bb1029e01d46a810f2664f
Madagasikara <m66@o2.pl>
2023-10-12 08:19:08 +0200

    Add project files.

---
+++ BasicOOP/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOOP
+{
+    internal class Circle
+    {
+
+        // property Radius
+        public int Radius { get; set; }
+        
+        // constructor
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+        
+        // the one and only method
+        public double GetArea()
+        {
+            double area = Math.PI * Math.Pow(Radius, 2);
+            Console.WriteLine($"Circle with radius {Radius} has an area of {area:0.##}");
+            return area;
+        }
+    }
+}
---
+++ BasicOOP/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BasicOOP
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            // Area calculations for circles with radiuses 5 and 6
+
+            Circle bestCircle = new Circle(6);
+            bestCircle.GetArea();
+
+            Circle worseCircle = new Circle(5);
+            worseCircle.GetArea();
+        }
+
+    }
+}
